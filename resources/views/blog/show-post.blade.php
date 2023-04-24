c31e0e27eeb5ed76e48bec96a0d8592c4a209da2
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2023-04-24 12:56:18 +0100

    ok

--- resources/views/blog/show-post.blade.php
+++ resources/views/blog/show-post.blade.php
@@ -14,5 +14,10 @@
         <div class="mt-5 text-left">
             @php echo $body->body @endphp
         </div>
+        <div>
+            <a href="https://www.facebook.com/sharer/sharer.php?u={{ Request::fullUrl() }}&display=popup" target="_blank" aria-label="Facebook"><i style="color:#3b5998" class="fab fa-2x fa-facebook"></i></a>
+            <a href="https://twitter.com/intent/tweet?text={{ $BlogArticle->title }}&url={{ Request::fullUrl() }}" target="_blank" aria-label="Twitter"><i style="color:dodgerblue" class="fab fa-2x fa-twitter"></i></a>
+            <a class="info" target="_blank" data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/?url={{ Request::fullUrl()}}; &media={{ asset('storage/'. $BlogArticle->main_image) }}&description={{ $BlogArticle->title }}" data-pin-custom="true" aria-label="Pinterest"><i style="color:crimson" class="fab fa-2x fa-pinterest"></i></i></a>
+        </div>
     </div>
 @endsection
