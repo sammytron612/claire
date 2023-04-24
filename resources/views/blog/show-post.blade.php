91e16027289cfd2cea2dab63d63345edd48a2ce8
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2023-04-24 13:31:56 +0100

    ok

--- resources/views/blog/show-post.blade.php
+++ resources/views/blog/show-post.blade.php
@@ -9,13 +9,22 @@
             <div class="col text-center">
                 <img class="w-100" src="{{$BlogArticle->main_image}}"/>
             </div>
-            <div class="col d-flex align-items-center"><h1 style="font-family: 'Pacifico', cursive;" class="mx-auto mt-5">{{$BlogArticle->title}}</h1></div>
+            <div class="col d-flex flex-column align-items-center justify-content-center">
+                
+                <div class="mt-3">
+                    <h6><i class="fa fa-lg fa-share-alt" aria-hidden="true"></i><span class="ml-2 weight700 text-teal">Share this</span></h6>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u={{ Request::fullUrl() }}&display=popup" target="_blank" aria-label="Facebook"><i style="color:#3b5998" class="fab fa-2x fa-facebook"></i></a>
+                    <a href="https://twitter.com/intent/tweet?text={{ $BlogArticle->title }}&url={{ Request::fullUrl() }}" target="_blank" aria-label="Twitter"><i style="color:dodgerblue" class="fab fa-2x fa-twitter"></i></a>
+                    <a class="info" target="_blank" data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/?url={{ Request::fullUrl()}}; &media={{ asset('storage/'. $BlogArticle->main_image) }}&description={{ $BlogArticle->title }}" data-pin-custom="true" aria-label="Pinterest"><i style="color:crimson" class="fab fa-2x fa-pinterest"></i></i></a>
+                </div>
+            </div>
+            
         </div>
         <div class="mt-5 text-left">
             @php echo $body->body @endphp
         </div>
         <div>
-            <h5>Share this</h5>
+            <h6><i class="fa fa-lg fa-share-alt" aria-hidden="true"></i><span class="ml-2 weight700 text-teal">Share this</span></h6>
             <a href="https://www.facebook.com/sharer/sharer.php?u={{ Request::fullUrl() }}&display=popup" target="_blank" aria-label="Facebook"><i style="color:#3b5998" class="fab fa-2x fa-facebook"></i></a>
             <a href="https://twitter.com/intent/tweet?text={{ $BlogArticle->title }}&url={{ Request::fullUrl() }}" target="_blank" aria-label="Twitter"><i style="color:dodgerblue" class="fab fa-2x fa-twitter"></i></a>
             <a class="info" target="_blank" data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/?url={{ Request::fullUrl()}}; &media={{ asset('storage/'. $BlogArticle->main_image) }}&description={{ $BlogArticle->title }}" data-pin-custom="true" aria-label="Pinterest"><i style="color:crimson" class="fab fa-2x fa-pinterest"></i></i></a>
